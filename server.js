aa5966a7494b25b0125739ab8f584860ab1ed3b1
James Stuckey <jstuckey@rutgers.edu>
2018-06-06 12:08:18 -0400

    travis CI space

--- server.js
+++ server.js
@@ -163,4 +163,4 @@ if (require.main === module) {
   runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
-module.exports = { app, runServer, closeServer };
+module.exports = { app, runServer, closeServer }; 
